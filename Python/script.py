b8407e4a20cfb963549bf46d8711200bfc5411b5
Jovan Ford <Jovan.ford14@gmail.com>
2022-05-23 11:28:42 -0400

    added

---
+++ Python/script.py
@@ -0,0 +1,13 @@
+def uniqueChar(phrase):
+  for x in phrase:
+     num = 0
+     phrase.sort
+     if x == x + 1:
+         num += 1
+         return False
+     else:
+         continue
+     if num == 0:
+        return True
+
+print(uniqueChar('jovan'))
