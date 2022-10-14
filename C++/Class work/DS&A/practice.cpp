2d90f88498cfc282029342b561589d578c947197
Jovan Ford <Jovan.ford14@gmail.com>
2022-10-14 02:57:55 -0400

    added

--- C++/Class work/DS&A/practice.cpp
+++ C++/Class work/DS&A/practice.cpp
@@ -1,41 +1,45 @@
-#include <iostream>
-#include <iomanip>
-using namespace std;
+// #include <iostream>
+// #include <iomanip>
+// #include <string>
+// using namespace std;
 
-void splitName(string &);
+// void splitName(string &);
 
-void print_with_commas(int n)
-{
-    int split = 1000;
+// void print_with_commas(int n)
+// {
+//     int split = 1000;
 
-    if (n < split)
-    {
-        cout << n;
-    }
-    else
-    {
-        int remainder = n % split;
-        print_with_commas(n / split);
-        cout << ',' << remainder;
-    }
-}
+//     if (n < split)
+//     {
+//         cout << n;
+//     }
+//     else
+//     {
+//         int remainder = n % split;
+//         print_with_commas(n / split);
+//         cout << ',' << remainder;
+//     }
+// }
 
-int main()
-{
-    string num = "123456789000";
-    string tempNum;
-    string finalNum;
-    unsigned int lol = num.size() - 3;
-    int v = 91000000;
-    auto s = std::to_string(v);
+// int main()
+// {
+//     int v = 91000000;
+//     auto s = std::to_string(v);
 
-    int n = s.length() - 3;
-    int end = (v >= 0) ? 0 : 1; // Support for negative numbers
-    while (n > 0)
-    {
-        s.insert(n, ",");
-        n -= 3;
-    }
+//     int n = s.length() - 3;
+//     int end = (v >= 0) ? 0 : 1; // Support for negative numbers
+//     while (n > 0)
+//     {
+//         s.insert(n, ",");
+//         n -= 3;
+//     }
+//     cout << s;
 
-    cout << s;
-}
+//     int number = 297353;
+//     string s = to_string(number);
+//     int len = s.size() - 3;
+//     while (number > 0){
+//         s.insert(n,',');
+//         n -= 3;
+//     }
+// }
